d711b81006e07cde0a9ef0e8c77515b6d3b80b42
Kevin Kang <kevinkang88@gmail.com>
2014-08-22 10:12:14 -0700

    initial commit with initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PLAYLTICS::Application.config.secret_token = '0597b91fa676c80c68ad33314b10f650b02578bb59b5ae762a4baa86404a343022b21ce0fc77d4f5893ea7580dfda8ef9285c17b58c115b8cfe04704a1fbb34e'
