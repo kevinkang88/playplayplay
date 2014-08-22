2c05177034dfe9162fdf973e8c77dae09804191f
Kevin Kang <kevinkang88@gmail.com>
2014-08-22 13:40:36 -0700

    added migration for the join table belongs to

--- db/migrate/20140822202031_create_playlists_tracks.rb
+++ db/migrate/20140822202031_create_playlists_tracks.rb
@@ -1,7 +1,8 @@
 class CreatePlaylistsTracks < ActiveRecord::Migration
   def change
     create_table :playlists_tracks do |t|
-
+      t.belongs_to :playlist
+      t.belongs_to :track
       t.timestamps
     end
   end
