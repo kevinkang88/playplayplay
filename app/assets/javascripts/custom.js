51c862ce8eb056f5ca7b3d3bb810601f631cee72
Kevin Kang <kevinkang88@gmail.com>
2014-08-31 21:11:37 -0700

    removed custom js file

--- app/assets/javascripts/custom.js
+++
@@ -1,7 +0,0 @@
-// this does not get fired but instead is located insdie result partial
-$(document).ready(function(){
-  $(".pagination").customPaginate({
-    itemsToPaginate : ".track-result"
-  });
-});
-
